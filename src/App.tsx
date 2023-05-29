5e149857431f95de5d99ee864ae3c4827a300b13
moskalyk <morgan.moskalyk@protonmail.ch>
2023-05-29 15:16:57 -0400

    removed /

--- src/App.tsx
+++ src/App.tsx
@@ -323,7 +323,7 @@ function App() {
   }
 
   const mint = (type: any, address: any) => {
-    fetch("http://216.128.178.48/:4000/transaction", {
+    fetch("http://216.128.178.48:4000/transaction", {
             method: "POST",
             headers: {
               "content-type": "application/json",

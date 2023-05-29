c4e277eb7ff361b1d4c6ff1f2d5edd567993c247
moskalyk <morgan.moskalyk@protonmail.ch>
2023-05-29 15:13:24 -0400

    updated cors

--- backend/server.ts
+++ backend/server.ts
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 4000
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: 'https://round-mouse-4296.on.fleek.co',
 };
   
 app.use(cors(corsOptions));
